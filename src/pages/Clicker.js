9b0ee091e68ab8193a288d63ce5f37e6688f89a1
Emre Baran KARACA <emre.b.karaca@hotmail.com>
2022-07-11 20:38:09 +0300

    reorganised the button,useeffect exc

--- src/pages/Clicker.js
+++ src/pages/Clicker.js
@@ -1,19 +1,33 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const Clicker = () => {
-  const [count,setCount] = useState(31);
+  const [count, setCount] = useState(31);
+
+  function incrementCount() {
+    setCount(count + 1) // neden 2 kere çağırınca yine 1 artıyor.. TAMAM DECREASE'DE  HALLETTİM İBRET OLSUN DİYE KALSIN.
+    setCount(count + 1)
+  }
+  function decreaseCount() {
+    setCount(count => count - 1 )
+    setCount(count => count - 1 )
+
+  }
 
   return (
-  <><><button onClick={() => setCount(count + 1)}>
-      <h1>Increase</h1>
-    </button><button onClick={() => setCount(count - 1)}>
-        <h1>Decrease  </h1>
+    <>
+      <button onClick={incrementCount}>
+        <h1>Increase by 1 </h1>
+      </button>
+
+      <button onClick={decreaseCount}>
+        <h1>Decrease by 2</h1>
       </button>
       <br></br>
-      </><span> <h1>{count}</h1></span></>
-  
-    )
+      <span> <h1>{count}</h1></span>
+
+    </>
+
+  )
 }
 
 export default Clicker
-  
