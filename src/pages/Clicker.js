515bf916e92cbf751b0243ca2b181d8bed68474e
Emre Baran KARACA <emre.b.karaca@hotmail.com>
2022-07-11 20:19:10 +0300

    organise the clicker

--- src/pages/Clicker.js
+++ src/pages/Clicker.js
@@ -1,11 +1,17 @@
 import React, {useState} from 'react'
 
 const Clicker = () => {
-  const [count,setCount] = useState(0);
+  const [count,setCount] = useState(31);
+
   return (
-  <button onClick = { ()=> setCount((count)=>(count+1))} >
-    <h1>You clicked {count} times, satisfied?</h1>
-  </button>
+  <><><button onClick={() => setCount(count + 1)}>
+      <h1>Increase</h1>
+    </button><button onClick={() => setCount(count - 1)}>
+        <h1>Decrease  </h1>
+      </button>
+      <br></br>
+      </><span> <h1>{count}</h1></span></>
+  
     )
 }
 
